2bd4e3883f40892f4ac6b637364726d3cbc005c8
Wilson Castro <wilsoncastro0710@gmail.com>
2021-01-11 13:34:38 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "build"),
+    path: path.join(__dirname, "public"),
     filename: "blunde.js",
   },
   devServer: {
