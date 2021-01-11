b251d83e2c27aca46120928bb92fdf86dc9eed91
Wilson Castro <wilsoncastro0710@gmail.com>
2021-01-11 14:20:33 -0500

    cambios

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,66 +1,61 @@
-import React, { Component } from 'react';
-import MenuNavigation from '../MenuNavigation/MenuNavigation';
-import MenuProfile from '../MenuProfile/MenuProfile';
-import Home from '../Home/Home';
-import Sellers from '../Sellers/Sellers';
-import Zones from '../Zones/Zones';
-import Cities from '../City/City';
-import Customers from '../Customers/Customers';
-import Login from '../Login/Login';
-import Product from '../Products/Products';
-import OrderCustomer from '../OrderCustomer/OrderCustomer';
+import React, { Component } from "react";
+import MenuNavigation from "../MenuNavigation/MenuNavigation";
+import MenuProfile from "../MenuProfile/MenuProfile";
+import Home from "../Home/Home";
+import Sellers from "../Sellers/Sellers";
+import Zones from "../Zones/Zones";
+import Cities from "../City/City";
+import Customers from "../Customers/Customers";
+import Login from "../Login/Login";
+import Product from "../Products/Products";
+import OrderCustomer from "../OrderCustomer/OrderCustomer";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
-import '../../../primereact/resources/themes/bootstrap4-light-blue/theme.css';
-import '../../../primereact/resources/primereact.min.css';
-import '../../../primeicons/primeicons.css';
+import "../../../primereact/resources/themes/bootstrap4-light-blue/theme.css";
+import "../../../primereact/resources/primereact.min.css";
+import "../../../primeicons/primeicons.css";
 
-import './Main.css';
+import "./Main.css";
 class Main extends Component {
-    
-    RenderViews() {
-        if (window.location.pathname.toLowerCase() == "/login") {
-            if(JSON.parse(localStorage.getItem('strJsonUser'))){
-                window.location.href='/';
-                return null;
-            }
-            return (<Login />);
-        } else {
-            if(!JSON.parse(localStorage.getItem('strJsonUser'))){
-                window.location.href='/Login';
-                return null;
-            }
-            if(JSON.parse(localStorage.getItem('strJsonUser')).intIdTypeUser==2){
-                if (window.location.pathname.toLowerCase() == "/") {
-                    window.location.href='/CreateOrder';
-                    return null;
-                }
-            }
-            return (
-                <section>
-                    <MenuProfile />
-                    <MenuNavigation />
-                    <BrowserRouter>
-                        <Switch>
-                            <Route exact path='/' component={Home} />
-                            <Route exact path="/Sellers" component={Sellers} />
-                            <Route exact path="/Zones" component={Zones} />
-                            <Route exact path="/Cities" component={Cities} />
-                            <Route exact path="/Customers" component={Customers} />
-                            <Route exact path="/Products" component={Product}/>
-                            <Route exact path="/CreateOrder" component={OrderCustomer}/>
-                        </Switch>
-                    </BrowserRouter>
-                </section>
-            );
+  RenderViews() {
+    if (window.location.pathname.toLowerCase() == "/login") {
+      if (JSON.parse(localStorage.getItem("strJsonUser"))) {
+        window.location.href = "/";
+        return null;
+      }
+      return <Login />;
+    } else {
+      if (!JSON.parse(localStorage.getItem("strJsonUser"))) {
+        window.location.href = "/Login";
+        return null;
+      }
+      if (JSON.parse(localStorage.getItem("strJsonUser")).intIdTypeUser == 2) {
+        if (window.location.pathname.toLowerCase() == "/") {
+          window.location.href = "/CreateOrder";
+          return null;
         }
+      }
+      return (
+        <section>
+          <MenuProfile />
+          <MenuNavigation />
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/Sellers" component={Sellers} />
+              <Route exact path="/Zones" component={Zones} />
+              <Route exact path="/Cities" component={Cities} />
+              <Route exact path="/Customers" component={Customers} />
+              <Route exact path="/Products" component={Product} />
+              <Route exact path="/CreateOrder" component={OrderCustomer} />
+            </Switch>
+          </BrowserRouter>
+        </section>
+      );
     }
-    render() {
-        return (
-            <main>
-                {this.RenderViews()}
-            </main>
-        );
-    }
+  }
+  render() {
+    return <main>{this.RenderViews()}</main>;
+  }
 }
 
 export default Main;
--- src/Components/MenuNavigation/MenuNavigation.js
+++ src/Components/MenuNavigation/MenuNavigation.js
@@ -1,89 +1,92 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    BsFillHouseDoorFill, BsFillPersonFill, BsFillGrid3X3GapFill,
-    BsFillFlagFill, BsFillArchiveFill, BsFillPeopleFill
+  BsFillHouseDoorFill,
+  BsFillPersonFill,
+  BsFillGrid3X3GapFill,
+  BsFillFlagFill,
+  BsFillArchiveFill,
+  BsFillPeopleFill,
 } from "react-icons/bs";
 
-import './MenuNavigation.css';
+import "./MenuNavigation.css";
 class MenuNavigation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            strDataUser: JSON.parse(localStorage.getItem('strJsonUser'))
-        }
-    }
-    //DeterminarMenu
-    Menu = () => {
-        try {
-            if (this.state.strDataUser.intIdTypeUser == 2) {
-                return (
-                    <li>
-                        <a href="/CreateOrder">
-                            <BsFillPeopleFill />
-                            <small>Create Order</small>
-                        </a>
-                    </li>
-                );
-            } else {
-                return (
-                    <span>
-                        <li>
-                            <a href="/">
-                                <BsFillHouseDoorFill />
-                                <small>Home</small>
-                            </a>
-                        </li>
-                        {/*<li>
+  constructor(props) {
+    super(props);
+    this.state = {
+      strDataUser: JSON.parse(localStorage.getItem("strJsonUser")),
+    };
+  }
+  //DeterminarMenu
+  Menu = () => {
+    try {
+      if (this.state.strDataUser.intIdTypeUser == 2) {
+        return (
+          <li>
+            <a href="/CreateOrder">
+              <BsFillPeopleFill />
+              <small>Create Order</small>
+            </a>
+          </li>
+        );
+      } else {
+        return (
+          <span>
+            <li>
+              <a href="/">
+                <BsFillHouseDoorFill />
+                <small>Home</small>
+              </a>
+            </li>
+            {/*<li>
                             <a href="/Order">
                                 <BsFillPeopleFill />
                                 <small>Order</small>
                             </a>
                         </li>*/}
-                        <li>
-                            <a href="/Sellers">
-                                <BsFillPersonFill />
-                                <small>Sellers</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/Zones">
-                                <BsFillGrid3X3GapFill />
-                                <small>Zones</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/Cities">
-                                <BsFillFlagFill />
-                                <small>Cities</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/Products">
-                                <BsFillArchiveFill />
-                                <small>Products</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/Customers">
-                                <BsFillPeopleFill />
-                                <small>Customers</small>
-                            </a>
-                        </li>
-                    </span>);
-            }
-        } catch (Error) {
-            console.log(Error)
-        }
-    }
-    render() {
-        return (
-            <nav className='Container-MenuNavigation'>
-                <ul>
-                    {this.Menu()}
-                </ul>
-            </nav>
+            <li>
+              <a href="/Sellers">
+                <BsFillPersonFill />
+                <small>Sellers</small>
+              </a>
+            </li>
+            <li>
+              <a href="/Zones">
+                <BsFillGrid3X3GapFill />
+                <small>Zones</small>
+              </a>
+            </li>
+            <li>
+              <a href="/Cities">
+                <BsFillFlagFill />
+                <small>Cities</small>
+              </a>
+            </li>
+            <li>
+              <a href="/Products">
+                <BsFillArchiveFill />
+                <small>Products</small>
+              </a>
+            </li>
+            <li>
+              <a href="/Customers">
+                <BsFillPeopleFill />
+                <small>Customers</small>
+              </a>
+            </li>
+          </span>
         );
+      }
+    } catch (Error) {
+      console.log(Error);
     }
+  };
+  render() {
+    return (
+      <nav className="Container-MenuNavigation">
+        <ul>{this.Menu()}</ul>
+      </nav>
+    );
+  }
 }
 
 export default MenuNavigation;
