3b9db9a0fd3eabffe5c963710759a0a424e409b7
Wilson Castro <wilsoncastro0710@gmail.com>
2021-01-11 09:08:46 -0500

    cambios

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,89 @@
-const path = require('path');
-const HtmlWebpackPlugin= require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
-module.exports={
-    entry:"./src/index.js",
-    output:{
-        path:path.join(__dirname,'build'),
-        filename:'blunde.js'
-    },
-    devServer:{
-        host:'192.168.1.56',
-        port:3001,
-        contentBase:':/dist',
-        disableHostCheck:true,
-        stats:{colors:true},
-        historyApiFallback:true
-    },
-    resolve:{
-        extensions: ['.js','.jsx','.css','jpg','png','.less','.scss','.styl']
-    },
-    module:{
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "blunde.js",
+  },
+  devServer: {
+    host: "192.168.1.56",
+    port: 3001,
+    contentBase: ":/dist",
+    disableHostCheck: true,
+    stats: { colors: true },
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: [
+      ".js",
+      ".jsx",
+      ".css",
+      "jpg",
+      "png",
+      ".less",
+      ".scss",
+      ".styl",
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(css)$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
             },
-            {
-                test: /\.(css)$/,
-                exclude: /node_modules/,
-                use: [
-                'style-loader',
-                {
-                    loader: 'css-loader',
-                    options:{
-                        importLoaders:1
-                    }
-                }]
+          },
+        ],
+      },
+      {
+        test: /\.(less)$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(scss)$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(styl)$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        exclude: /node_modules/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
             },
-            {
-                test: /\.(less)$/,
-                exclude: /node_modules/,
-                use: ['style-loader',
-                    'css-loader',
-                    'less-loader']
-            },
-            {
-                test: /\.(scss)$/,
-                exclude: /node_modules/,
-                use: ['style-loader',
-                    'css-loader',
-                    'sass-loader']
-            },
-            {
-                test: /\.(styl)$/,
-                exclude: /node_modules/,
-                use: ['style-loader',
-                    'css-loader',
-                    'stylus-loader']
-            },
-            {
-                test: /\.(jpg|png)$/,
-                exclude: /node_modules/,
-                use: ['file-loader']
-            }
-            ,
-            {
-                  test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                    use: [
-                      {
-                        loader: 'file-loader',
-                        options: {
-                          name: '[name].[ext]',
-                          outputPath: 'fonts/'
-                        }
-                      }
-                    ]
-                  }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-    ]
-}
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
